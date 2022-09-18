8f46ae61e1b814ee80ab3cc47fed5877f622e43c
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-09-18 20:19:35 +0300

    Добавила прогу Задача 2 Семинар 1

---
+++ Seminar/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два 
+// числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.WriteLine("Введите значение первого числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение второго числа: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int max=0;
+int min=0;
+
+if(n>m) 
+{
+    max=n; min=m;
+    Console.Write("max = ");
+    Console.Write(max);
+    Console.Write(", min = ");
+    Console.Write(min);
+}
+if(m>n)
+{
+    max=m; min=n;
+    Console.Write("max = ");
+    Console.Write(max);
+    Console.Write(", min = ");
+    Console.Write(min);
+}
